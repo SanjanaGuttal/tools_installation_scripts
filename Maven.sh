0e01c6528e378256e0ec2fae99ba1719659f57a1
Sanjana Guttal <65895331+SanjanaGuttal@users.noreply.github.com>
2024-02-17 15:50:14 +0530

    Update Maven.sh

--- Maven.sh
+++ Maven.sh
@@ -1,3 +1,10 @@
+cd /opt/
+wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz tar xvzf apache-maven-3.6.3-bin.tar.gz
+vi /etc/profile.d/maven.sh
+export MAVEN_HOME=/opt/apache-maven-3.6.3 export PATH=$PATH:$MAVEN_HOME/bin
+
+and or or
+
 sudo apt update -y
 sudo apt install maven -y
 mvn -version
